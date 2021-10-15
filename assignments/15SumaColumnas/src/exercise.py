5bbd98704f70f40b567e9c0ed341029e4739a51a
SofiaMejia <A01252540@itesm.mx>
2021-10-15 17:45:48 +0000

    15 suma columnas

--- assignments/15SumaColumnas/src/exercise.py
+++ assignments/15SumaColumnas/src/exercise.py
@@ -1,8 +1,27 @@
-
+def crearmatriz():
+    listaa = []
+    fila = int(input())
+    columna = int(input())
+    if fila > 0 and columna > 0: 
+        for i in range(fila):
+            listaa.append([])
+            for j in range(columna):
+                n = int(input())
+                listaa[i].append(n)
+    else: 
+        print('Error')
+    return listaa
 
 def main():
-
-
-
+    matriz = crearmatriz()
+    listaa_col = []
+    if len(matriz) > 0:
+        for i in range(len(matriz[0])):
+            count = 0
+            for j in range(len(matriz)):
+                count += matriz[j][i] 
+            listaa_col.append(count)
+        print(listaa_col)
 if __name__=='__main__':
     main()
+
